d6101ae7d469cfc8b474f27a3a4b8b503a967644
shmuelko <shmuel777@gmail.com>
2020-07-08 20:06:07 +0300

    remove DOM and save files

--- src/practitest_firecracker/core.clj
+++ src/practitest_firecracker/core.clj
@@ -7,7 +7,7 @@
                                               populate-sf-results
                                               create-or-update-sf-testset]]
    [practitest-firecracker.surefire   :refer [parse-reports-dir]]
-   [test-xml-parser.core              :refer [send-directory remove-BOM]]
+   [test-xml-parser.core              :refer [send-directory remove-bom return-files]]
    [clojure.pprint                    :as    pprint]
    [clojure.java.io                   :refer [file]])
   (:gen-class))
@@ -28,51 +28,47 @@
   (let [{:keys [action options exit-message ok?]} (parse-args args)]
     (if exit-message
       (exit (if ok? 0 1) exit-message)
-      (let [client  (make-client (select-keys options [:email :api-token :api-uri :max-api-rate]))
-            ;; val     (fix-BOM (file (first (:reports-path options))))
-            reports (parse-reports-dir (:reports-path options))
-            ;; config  (parse-reports-dir (:reports-path options))
-            directory (file (first (:reports-path options)))
-            ;; additional-config  (send-directory directory config)
-            additional-reports (send-directory directory reports)]
-        (case action
-          "display-config"
-          (let [result (send-directory directory reports)]
-            (pprint/pprint "value:")
-            (pprint/pprint val)
-            ;; (pprint/pprint {"=============== additional-config: ===============" additional-config})
-            (pprint/pprint {"=============== additional-reports: ===============" additional-reports})
-            (pprint/pprint {"=============== FC original reports val: ===============" reports})
-            (pprint/pprint {"=============== FC result: ===============" result}))
+      (do
+        (remove-bom (file (first (:reports-path options))))
+        (let [client             (make-client (select-keys options [:email :api-token :api-uri :max-api-rate]))
+              reports            (parse-reports-dir (:reports-path options))
+              directory          (file (first (:reports-path options)))
+              additional-reports (send-directory directory reports)]
+          (case action
+            "display-config"
+            (let [result (send-directory directory reports)]
+              (pprint/pprint {"=============== additional-reports: ===============" additional-reports})
+              (pprint/pprint {"=============== FC original reports val: ===============" reports})
+              (pprint/pprint {"=============== FC result: ===============" result}))
 
-          "display-options"
-          (do
-            (pprint/pprint {"=============== options: ===============" options})
-            (pprint/pprint {"=============== args: ===============" args}))
+            "display-options"
+            (do
+              (pprint/pprint {"=============== options: ===============" options})
+              (pprint/pprint {"=============== args: ===============" args}))
 
-          "create-testset"
-          (let [testset (create-or-update-sf-testset client options additional-reports)]
-            (exit 0 (format "TestSet ID: %s" (:id testset))))
+            "create-testset"
+            (let [testset (create-or-update-sf-testset client options additional-reports)]
+              (exit 0 (format "TestSet ID: %s" (:id testset))))
 
-          "populate-testset"
-          (do
-            (populate-sf-results client
-                                 options
-                                 additional-reports)
-            (exit 0 "Done"))
+            "populate-testset"
+            (do
+              (populate-sf-results client
+                                   options
+                                   additional-reports)
+              (exit 0 "Done"))
 
-          "create-and-populate-testset"
-          (let [testset (timef
-                         "create-or-update-testset"
-                         (create-or-update-sf-testset client options additional-reports))]
-            (timef
-             "populate-results"
-             (populate-sf-results client
-                                  (assoc options
-                                         :skip-validation? true
-                                         :testset-id       (:id testset))
-                                  additional-reports))
-            (exit 0 (format "Populated TestSet ID: %s" (:id testset))))
-          "test"
-          (let [testset (create-or-update-sf-testset client options additional-reports additional-reports)]
-            (exit 0 (format "TestSet ID: %s" (:id testset)))))))))
+            "create-and-populate-testset"
+            (let [testset (timef
+                           "create-or-update-testset"
+                           (create-or-update-sf-testset client options additional-reports))]
+              (timef
+               "populate-results"
+               (populate-sf-results client
+                                    (assoc options
+                                           :skip-validation? true
+                                           :testset-id       (:id testset))
+                                    additional-reports))
+              (exit 0 (format "Populated TestSet ID: %s" (:id testset))))
+            "test"
+            (let [testset (create-or-update-sf-testset client options additional-reports additional-reports)]
+              (exit 0 (format "TestSet ID: %s" (:id testset))))))))))
