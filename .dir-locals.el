e5cc927e212756507cf0952c285a92db2369423d
shmuelko <shmuel777@gmail.com>
2021-08-24 23:32:40 +0300

    change dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,5 @@
-((nil . ((cider-clojure-cli-aliases . "dev"))))
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (cider-clojure-cli-global-options . "-A:dev")))
