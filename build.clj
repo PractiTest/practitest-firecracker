2cfd062c710874adbbdba263bedd86fea6519577
Denis Shilov <denis@practitest.com>
2024-11-28 15:40:51 +0500

    Changed version calculation code a bit
    Added output to see what's going on during uberjar build process

--- build.clj
+++ build.clj
@@ -3,7 +3,13 @@
     [clojure.tools.build.api :as b]))
 
 (def lib 'practitest/practitest-firecracker)
-(def version (format "2.2.%s" (b/git-count-revs nil)))
+
+; Create new tag v<Major>.<Minor> when starting working on new major/minor version branch
+(def major-version 2)
+(def minor-version 2)
+
+(def commits-count (b/git-process {:git-args (format "rev-list v%s.%s..HEAD --count" major-version minor-version)}))
+(def version (format "%s.%s.%s" major-version minor-version commits-count))
 (def class-dir "target/classes")
 (def uber-file (format "target/%s-%s-standalone.jar" (name lib) version))
 
@@ -11,17 +17,21 @@
 (def basis (delay (b/create-basis {:project "deps.edn"})))
 
 (defn clean [_]
+  (println "Cleaning directories")
   (b/delete {:path "target"}))
 
 (defn uber [_]
+  (println "Building uberjar for" version)
   (clean nil)
   (b/copy-dir {:src-dirs ["src"]
                :target-dir class-dir})
   (b/copy-dir {:src-dirs ["resources"]
                :target-dir class-dir})
+  (println "Compiling clojure code")
   (b/compile-clj {:basis @basis
                   :ns-compile '[practitest-firecracker.core]
                   :class-dir class-dir})
+  (println "Packing JAR file")
   (b/uber {:class-dir class-dir
            :uber-file uber-file
            :basis @basis
